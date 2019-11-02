ab905b9bd0dd4b32cfc42128ce357d211f7ebd88
ahoho <ahoyle@wesleyan.edu>
2019-11-02 16:17:03 -0400

    turn read_jsonlist into a generator

--- file_handling.py
+++ file_handling.py
@@ -17,17 +17,15 @@ def write_to_json(data, output_filename, indent=2, sort_keys=True):
 
 
 def read_json(input_filename):
-    with codecs.open(input_filename, 'r', encoding='utf-8') as input_file:
+    with open(input_filename, 'r', encoding='utf-8') as input_file:
         data = json.load(input_file, encoding='utf-8')
     return data
 
 
 def read_jsonlist(input_filename):
-    data = []
-    with codecs.open(input_filename, 'r', encoding='utf-8') as input_file:
+    with open(input_filename, 'r', encoding='utf-8') as input_file:
         for line in input_file:
-            data.append(json.loads(line, encoding='utf-8'))
-    return data
+            yield json.loads(line, encoding='utf-8')
 
 
 def write_jsonlist(list_of_json_objects, output_filename, sort_keys=True):
