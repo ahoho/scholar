769f7f29a75cdedfb5e31a09d7705b2db0a949e3
ahoho <ahoyle@wesleyan.edu>
2019-12-04 19:38:22 -0500

    compute npmi while creating results

--- make_results_table.py
+++ make_results_table.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from collections import defaultdict
 from pathlib import Path
 from datetime import datetime
@@ -6,6 +7,9 @@ from datetime import datetime
 import pandas as pd
 import tqdm
 import torch
+
+from compute_npmi import compute_npmi_at_n
+import file_handling as fh
 
 def read_result_from_file(fpath, fail_silently=True):
     """
@@ -17,20 +21,46 @@ def read_result_from_file(fpath, fail_silently=True):
     with open(fpath, 'r') as infile:
         return float(infile.read().strip())
 
-def get_results_data(basedir='./', pattern='output*', ignore_cols_with_same_vals=True):
+def get_results_data(
+    basedir,
+    pattern,
+    ignore_cols_with_same_vals=True,
+    coherence_reference_dir="/fs/clip-political/scholar/congress_votes_dwnom"
+):
     """
     Get the results data in folders matching `pattern` in `basedir`
     """
     dirs = [(p.name, p) for p in Path(basedir).glob(pattern) if p.is_dir()]
 
+    ref_vocab = fh.read_json(Path(coherence_reference_dir, "train.vocab.json"))
+    ref_counts = fh.load_sparse(Path(coherence_reference_dir, "test.npz")).tocsc()
+
     experiments = pd.DataFrame()
     column_names = []
     for run_name, run_dir in tqdm.tqdm(dirs):
+
         model_path = Path(run_dir, 'torch_model.pt')
         try:
             checkpoint = torch.load(model_path, map_location='cpu')
         except FileNotFoundError:
             continue
+
+        
+        npmi_internal = None
+        try:
+            topics = fh.read_text(Path(run_dir, "topic.txt"))
+        except FileNotFoundError:
+            print(f"topics.txt not found for {run_name}. Will not calculate npmi")
+            pass
+        else:
+            npmi_internal = compute_npmi_at_n(
+                topics=topics,
+                ref_vocab=ref_vocab,
+                ref_counts=ref_counts,
+                n=10, # could change?
+                silent=True,
+            )
+
         model_time = (
             datetime.fromtimestamp(model_path.stat().st_mtime).strftime('%Y-%m-%d %H:%M')
         )
@@ -74,20 +104,32 @@ def get_results_data(basedir='./', pattern='output*', ignore_cols_with_same_vals
     return experiments.sort_values(by=['date'])
 
 if __name__ == '__main__':
-    results_data = get_results_data()
-    results_data.to_csv('results.csv', index=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base-directory', default='./')
+    parser.add_argument('--file-pattern', default='output*')
+    parser.add_argument('--ouput-fpath', '-o', default='results.csv')
+
+    # google sheet information
+    parser.add_argument('--do-not-save-to-sheets', action='store_false')
+    parser.add_argument('--google-auth-key-dir', default="~/scholar-experiments-key/")
+    parser.add_argument('--google-sheets-name', default='scholar-experiments')
+    parser.add_argument('--share-with-users', nargs='+', default=[])
+
+    args = parser.parse_args()
+
+    results_data = get_results_data(basedir=args.base_dir, pattern=args.file_pattern)
+    results_data.to_csv(args.output_fpath, index=False)
 
     # below code stores as google sheet
+    if args.do_not_save_to_sheets:
+        exit()
+    
     import gspread_pandas
 
-    GOOGLE_AUTH_KEY_DIRECTORY = "~/scholar-experiments-key/" # google_secret.json should be in this directory
-    RESULTS_SHEET_NAME = "scholar-experiments"
-    SHARE_WITH_USERS = ["hoyle@umd.edu", "pgoel1@cs.umd.edu"]
-
-    config = gspread_pandas.conf.get_config(GOOGLE_AUTH_KEY_DIRECTORY)
+    config = gspread_pandas.conf.get_config(args.google_auth_key_dir)
     client = gspread_pandas.Client(config=config)
     sheet = gspread_pandas.Spread(
-        spread=RESULTS_SHEET_NAME,
+        spread=args.google_sheets_name,
         client=client,
         create_spread=True,
     )
@@ -98,9 +140,9 @@ if __name__ == '__main__':
     sheet.df_to_sheet(results_data, index=False, replace=True, sheet='results')
     
     # sharing
-    access_sheet = client.open(RESULTS_SHEET_NAME)
+    access_sheet = client.open(args.google_sheets_name)
     authenticated_users = [p["emailAddress"] for p in access_sheet.list_permissions()]
-    for user in SHARE_WITH_USERS:
+    for user in args.share_with_users:
         if user not in authenticated_users:
             access_sheet.share(user, perm_type="user", role="writer")
 
